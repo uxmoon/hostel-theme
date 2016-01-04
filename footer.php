2d9efcdb84a0799e5c362ee9c98d3247f09999b5
Martin Luna <Martin Luna>
2016-01-04 20:46:13 -0300

    first commit

---
+++ footer.php
@@ -0,0 +1,98 @@
+<?php
+    $settings = get_option( 'my_options' );
+?>
+    </div><!-- #content -->
+
+    <footer id="colophon" class="site-footer" role="contentinfo">
+        <div class="site-footer--top">
+            <!-- weather widget -->
+            <?php dynamic_sidebar( 'sidebar-1' ); ?>
+
+            <!-- texto -->
+
+            <?php
+                $field_footer_media = 'general_footer_media';
+                if ( isset( $settings[$field_footer_media] ) )
+                {
+                    echo '<img src=' . $settings[$field_footer_media] . ' alt="Hosteria">';
+                }
+
+                $field_footer_text = 'general_footer_text_content';
+                if ( isset( $settings[$field_footer_text] ) ) {
+                    echo $settings[$field_footer_text];
+                }
+            ?>
+
+            <h4>Hágase fan</h4>
+
+            <?php
+                $field_fb_url   = 'general_fb_url';
+                $field_fb_label = 'general_fb_label';
+                if ( isset( $settings[$field_fb_url] ) ) {
+                    echo '<p>Síganos en <a href="' . $settings[$field_fb_url] . ' rel="external">'. $settings[$field_fb_label] . '</a>';
+                }
+            ?></p>
+        </div>
+        <div class="site-footer--botom">
+            <ul>
+
+                <?php
+                $field_email = 'general_email';
+                if ( isset( $settings[$field_email] ) ) {
+                    echo '<li>' . $settings[$field_email] . '</li>';
+                }
+
+                $field_address = 'general_address';
+                if ( isset( $settings[$field_address] ) ) {
+                    echo '<li>' . $settings[$field_address] . '</li>';
+                }
+
+                $field_phone = 'general_phone';
+                if ( isset( $settings[$field_phone] ) ) {
+                    echo '<li>' . $settings[$field_phone] . '</li>';
+                }
+
+                $field_cellphone = 'general_cellphone';
+                if ( isset( $settings[$field_cellphone] ) ) {
+                    echo '<li>' . $settings[$field_cellphone] . '</li>';
+                }
+
+                $field_fb_url   = 'general_fb_url';
+                $field_fb_label = 'general_fb_label';
+                if ( isset( $settings[$field_fb_url] ) ) {
+                    echo '<li><a href="' . $settings[$field_fb_url] . '">'. $settings[$field_fb_label] . '</a></li>';
+                }
+
+                // $field_tw_url   = 'general_tw_url';
+                // $field_tw_label = 'general_tw_label';
+                // if ( isset( $settings[$field_tw_url] ) ) {
+                //     echo '<li><a href="' . $settings[$field_tw_url] . '">'. $settings[$field_tw_label] . '</a></li>';
+                // }
+
+                // $field_gp_url   = 'general_gp_url';
+                // $field_gp_label = 'general_gp_label';
+                // if ( isset( $settings[$field_gp_url] ) ) {
+                //     echo '<li><a href="' . $settings[$field_gp_url] . '">'. $settings[$field_gp_label] . '</a></li>';
+                // }
+
+                ?>
+
+            </ul>
+            <?php wp_nav_menu( array( 'theme_location' => 'footer', 'menu_id' => 'footer-menu' ) ); ?>
+            <div class="site-info">
+                <p>&copy; Copyright <?php echo date('Y'); ?>. Hostería Los Leños. Todos los derechos reservados.</p>
+                <?php
+                $field_copy = 'general_copy';
+                if ( isset( $settings[$field_copy] ) ) {
+                    echo wpautop( $settings[$field_copy] );
+                }
+                ?>
+            </div><!-- .site-info -->
+        </div>
+    </footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ inc/mb-settings-page.php
@@ -0,0 +1,155 @@
+<?php
+add_filter( 'mb_settings_pages', 'prefix_settings_pages' );
+function prefix_settings_pages( $settings_pages )
+{
+    $settings_pages[] = array(
+        'id'            => 'my-options',
+        'option_name'   => 'my_options',
+        'menu_title'    => __( 'Configuración', 'hosteria' ),
+        'icon_url'      => 'dashicons-admin-generic',
+        'submenu_title' => __( 'General', 'hosteria' ),
+    );
+    $settings_pages[] = array(
+        'id'            => 'my-options-footer',
+        'option_name'   => 'my_options',
+        'menu_title'    => __( 'Pie de página', 'hosteria' ),
+        'parent'        => 'my-options',
+    );
+    $settings_pages[] = array(
+        'id'            => 'my-options-copy',
+        'option_name'   => 'my_options',
+        'menu_title'    => __( 'Copyright', 'hosteria' ),
+        'parent'        => 'my-options',
+    );
+    return $settings_pages;
+}
+
+
+// Register settings meta boxes and fields
+add_filter( 'rwmb_meta_boxes', 'prefix_options_meta_boxes' );
+function prefix_options_meta_boxes( $meta_boxes )
+{
+    $meta_boxes[] = array(
+        'id'             => 'general',
+        'title'          => __( 'General', 'hosteria' ),
+        'settings_pages' => 'my-options',
+        'fields'         => array(
+            array (
+                'name'        => __( 'Logo', 'loslenos' ),
+                'id'          => 'general_logo',
+                'type'        => 'file_input',
+            ),
+            array (
+                'name'        => __( 'Dirección', 'loslenos' ),
+                'id'          => 'general_address',
+                'type'        => 'text',
+                'size'        => 60
+            ),
+            array (
+                'name'        => __( 'Email', 'loslenos' ),
+                'id'          => 'general_email',
+                'type'        => 'text',
+                'size'        => 30
+            ),
+            array (
+                'name'        => __( 'Teléfono', 'loslenos' ),
+                'id'          => 'general_phone',
+                'type'        => 'text',
+                'size'        => 30
+            ),
+            array (
+                'name'        => __( 'Celular', 'loslenos' ),
+                'id'          => 'general_cellphone',
+                'type'        => 'text',
+                'size'        => 30
+            ),
+            array (
+                'name'        => __( 'Facebook URL', 'loslenos' ),
+                'id'          => 'general_fb_url',
+                'type'        => 'text',
+                'size'        => 60
+            ),
+            array (
+                'name'        => __( 'Facebook texto', 'loslenos' ),
+                'id'          => 'general_fb_label',
+                'type'        => 'text',
+                'size'        => 30
+            ),
+            array (
+                'name'        => __( 'Twitter URL', 'loslenos' ),
+                'id'          => 'general_tw_url',
+                'type'        => 'text',
+                'size'        => 60
+            ),
+            array (
+                'name'        => __( 'Twitter texto', 'loslenos' ),
+                'id'          => 'general_tw_label',
+                'type'        => 'text',
+                'size'        => 30
+            ),
+            array (
+                'name'        => __( 'Google+ URL', 'loslenos' ),
+                'id'          => 'general_gp_url',
+                'type'        => 'text',
+                'size'        => 60
+            ),
+            array (
+                'name'        => __( 'Google+ texto', 'loslenos' ),
+                'id'          => 'general_gp_label',
+                'type'        => 'text',
+                'size'        => 30
+            ),
+        ),
+    );
+    return $meta_boxes;
+}
+
+add_filter( 'rwmb_meta_boxes', 'your_prefix_textarea_demo' );
+function your_prefix_textarea_demo( $meta_boxes )
+{
+    $meta_boxes[] = array(
+        'id'             => 'copyright',
+        'title'          => __( 'General', 'loslenos' ),
+        'settings_pages' => 'my-options-copy',
+        'fields'         => array(
+            array(
+                'name'        => __( 'Texto al pie de la página', 'loslenos' ),
+                'id'          => 'general_copy',
+                'type'        => 'textarea',
+                'rows'        => 5,
+                'cols'        => 5,
+            ),
+        ),
+    );
+    return $meta_boxes;
+}
+
+
+add_filter( 'rwmb_meta_boxes', 'your_prefix_footer_demo' );
+function your_prefix_footer_demo( $meta_boxes )
+{
+    $meta_boxes[] = array(
+        'id'             => 'footer',
+        'title'          => __( 'Información', 'loslenos' ),
+        'settings_pages' => 'my-options-footer',
+        'fields'         => array(
+            array(
+                'name' => __( 'Logo', 'loslenos' ),
+                'id'   => 'general_footer_media',
+                'type' => 'file_input',
+            ),
+            array(
+                'name'    => __( 'Texto', 'loslenos' ),
+                'id'      => "general_footer_text_content",
+                'type'    => 'wysiwyg',
+                'raw'     => false,
+                'options' => array(
+                    'textarea_rows' => 4,
+                    'teeny'         => true,
+                    'media_buttons' => false,
+                ),
+            ),
+        ),
+    );
+    return $meta_boxes;
+}
