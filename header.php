21859fb6e9ac7c2c57dc50c9789a0ad9da969c51
Martin Luna <martinluna@uxmoon.com>
2016-02-17 16:15:08 -0300

    updated head section markup and added wp tweaks to clean head section and admin

--- header.php
+++ header.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html class="no-js" <?php language_attributes(); ?>>
 <head>
+
 <meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 <?php wp_head(); ?>
+
 </head>
 
 <body <?php body_class(); ?>>
@@ -27,19 +30,19 @@
                 <p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
             <?php
             endif; ?>
-        </div><!-- .site-branding -->
+        </div>
 
         <nav id="site-navigation" class="main-navigation" role="navigation">
             <?php wp_nav_menu(
                 array(
                     'theme_location' => 'primary',
                     'menu_id'        => 'primary-menu',
-                    'container' => '',
-                    'menu_class' => 'site-menu'
+                    'container'      => '',
+                    'menu_class'     => 'site-menu'
                     )
                 );
             ?>
-        </nav><!-- #site-navigation -->
-    </header><!-- #masthead -->
+        </nav>
+    </header>
 
     <div id="content" class="site-content">
--- inc/wp-tweaks.php
+++ inc/wp-tweaks.php
@@ -1,35 +1,124 @@
 <?php
-// Disable Emojicons
-function disable_wp_emojicons() {
-
-  remove_action( 'admin_print_styles', 'print_emoji_styles' );
-  remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-  remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-  remove_action( 'wp_print_styles', 'print_emoji_styles' );
-  remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
-  remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
-  remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
-
-  add_filter( 'tiny_mce_plugins', 'disable_emojicons_tinymce' );
-}
-add_action( 'init', 'disable_wp_emojicons' );
-
-function disable_emojicons_tinymce( $plugins ) {
-  if ( is_array( $plugins ) ) {
-    return array_diff( $plugins, array( 'wpemoji' ) );
-  } else {
-    return array();
-  }
-}
+// disable emojis
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
 
 // Contact Form 7
 add_filter( 'wpcf7_load_js', '__return_false' );
 add_filter( 'wpcf7_load_css', '__return_false' );
-
-// WP GEO
-if( class_exists('WPGeo') && !is_admin() ){
-    remove_action( 'wp_enqueue_scripts', array( $wpgeo, 'enqueue_scripts' ) );
-    remove_action( 'wp_head', array( $wpgeo, 'meta_tags' ) );
-    remove_action( 'wp_head', array( $wpgeo, 'wp_head' ) );
-    remove_action( 'wp_footer', array( $wpgeo, 'wp_footer' ) );
+// post and comments
+// Disable comments globally - https://www.dfactory.eu/turn-off-disable-comments/
+// - - - - - - - - - - - - - - - - - - - - - - - - -
+function df_disable_comments_post_types_support() {
+    $post_types = get_post_types();
+    foreach ($post_types as $post_type) {
+        if(post_type_supports($post_type, 'comments')) {
+            remove_post_type_support($post_type, 'comments');
+            remove_post_type_support($post_type, 'trackbacks');
+        }
+    }
 }
+add_action('admin_init', 'df_disable_comments_post_types_support');
+
+
+// Close comments on the front-end
+// - - - - - - - - - - - - - - - - - - - - - - - - -
+function df_disable_comments_status() {
+    return false;
+}
+add_filter('comments_open', 'df_disable_comments_status', 20, 2);
+add_filter('pings_open', 'df_disable_comments_status', 20, 2);
+
+
+// Hide existing comments
+// - - - - - - - - - - - - - - - - - - - - - - - - -
+function df_disable_comments_hide_existing_comments($comments) {
+    $comments = array();
+    return $comments;
+}
+add_filter('comments_array', 'df_disable_comments_hide_existing_comments', 10, 2);
+
+
+// Redirect any user trying to access comments page
+// - - - - - - - - - - - - - - - - - - - - - - - - -
+function df_disable_comments_admin_menu_redirect() {
+    global $pagenow;
+    if ($pagenow === 'edit-comments.php') {
+        wp_redirect(admin_url()); exit;
+    }
+}
+add_action('admin_init', 'df_disable_comments_admin_menu_redirect');
+
+
+// Remove comments metabox from dashboard
+// - - - - - - - - - - - - - - - - - - - - - - - - -
+function df_disable_comments_dashboard() {
+    remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+}
+add_action('admin_init', 'df_disable_comments_dashboard');
+
+
+// Remove comments links from admin bar
+// - - - - - - - - - - - - - - - - - - - - - - - - -
+function df_disable_comments_admin_bar() {
+    if (is_admin_bar_showing()) {
+        remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu', 60);
+    }
+}
+add_action('init', 'df_disable_comments_admin_bar');
+
+// Remove sidebar links
+// - - - - - - - - - - - - - - - - - - - - - - - - -
+function remove_menus(){
+  remove_menu_page( 'edit.php' );
+  remove_menu_page( 'edit-comments.php' );
+}
+add_action( 'admin_menu', 'remove_menus' );
+
+// remove junk from head
+remove_action('wp_head', 'rsd_link');
+remove_action('wp_head', 'wp_generator'); //removes WP Version # for security
+remove_action('wp_head', 'feed_links', 2);
+remove_action('wp_head', 'index_rel_link');
+remove_action('wp_head', 'wlwmanifest_link');
+remove_action('wp_head', 'feed_links_extra', 3);
+remove_action('wp_head', 'start_post_rel_link', 10, 0);
+remove_action('wp_head', 'parent_post_rel_link', 10, 0);
+remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
+remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0 );
+remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
+
+// remove wp version param from any enqueued scripts
+function vc_remove_wp_ver_css_js( $src ) {
+    if ( strpos( $src, 'ver=' . get_bloginfo( 'version' ) ) )
+        $src = remove_query_arg( 'ver', $src );
+    return $src;
+}
+add_filter( 'style_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
+add_filter( 'script_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
+
+// Remove the WordPress Logo from the Toolbar
+function remove_wp_logo( $wp_admin_bar ) {
+  $wp_admin_bar->remove_node( 'wp-logo' );
+}
+add_action( 'admin_bar_menu', 'remove_wp_logo', 999 );
+
+// disable the admin bar
+show_admin_bar(false);
+
+// remove links/menus from the admin bar
+function mytheme_admin_bar_render() {
+  global $wp_admin_bar;
+  $wp_admin_bar->remove_menu('comments');
+  $wp_admin_bar->remove_menu('new-content');
+}
+add_action( 'wp_before_admin_bar_render', 'mytheme_admin_bar_render' );
+// WP GEO
+// if( class_exists('WPGeo') && !is_admin() ){
+//     remove_action( 'wp_enqueue_scripts', array( $wpgeo, 'enqueue_scripts' ) );
+//     remove_action( 'wp_head', array( $wpgeo, 'meta_tags' ) );
+//     remove_action( 'wp_head', array( $wpgeo, 'wp_head' ) );
+//     remove_action( 'wp_footer', array( $wpgeo, 'wp_footer' ) );
+// }
