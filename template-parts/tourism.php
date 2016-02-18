8558753557862ab03944d0ef2548536b07b49df4
Martin Luna <martinluna@uxmoon.com>
2016-02-18 11:22:07 -0300

    added sidebar menu for tourism child pages

--- template-parts/tourism.php
+++ template-parts/tourism.php
@@ -14,6 +14,15 @@ get_header(); ?>
                     <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
                 </header>
                 <div class="entry-content">
+                <?php wp_nav_menu(
+                    array(
+                        'theme_location' => 'sidebar',
+                        'menu_id'        => '',
+                        'container'      => '',
+                        // 'menu_class'     => ''
+                        )
+                    );
+                ?>
                     <?php the_content(); ?>
                 </div>
             </article>
