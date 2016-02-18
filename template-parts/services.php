98cef8e8b7c4fe572c6e26bb96fc4870867c24fd
Martin Luna <martinluna@uxmoon.com>
2016-02-18 10:57:35 -0300

    removed unused content pages

--- template-parts/services.php
+++ template-parts/services.php
@@ -7,12 +7,63 @@ get_header(); ?>
     <div id="primary" class="content-area">
         <main id="main" class="site-main" role="main">
 
-            <?php
-            while ( have_posts() ) : the_post();
+            <?php while ( have_posts() ) : the_post();  ?>
 
-                get_template_part( 'template-parts/content', 'page-services' );
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <header class="entry-header">
+                    <div class="row">
+                        <div class="small-12 columns">
+                            <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+                        </div>
+                    </div>
+                </header>
+                <div class="entry-content">
+                    <div class="row">
+                        <div class="small-12 columns">
+                            <?php the_content(); ?>
+                        </div>
+                    </div>
 
-            endwhile; ?>
+                    <!-- services other -->
+                    <div class="hl-row">
+                        <div class="row">
+                            <div class="small-12 columns">
+                            <?php echo rwmb_meta( 'services_tourism_other' ); ?>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="small-12 columns">
+                        <!-- services tourism -->
+                        <?php
+                            $images = rwmb_meta( 'services_tourism_media', 'type=image&size=full' );
+                            foreach ( $images as $image )
+                            {
+                                echo "<img src='{$image['url']}' width='{$image['width']}' height='{$image['height']}' alt='{$image['alt']}' />";
+                            }
+                        ?>
+                        <?php echo rwmb_meta( 'services_tourism_text' ); ?>
+
+                        <!-- services pilars -->
+                        <div class="hl-pilars">
+                            <div class="row">
+                                <div class="small-12 medium-6 columns">
+                                    <?php echo rwmb_meta( 'services_pilars_text' ); ?>
+                                </div>
+                                <div class="small-12 medium-6 columns">
+                                    <div class="hl-pilars-last">
+                                        <?php echo rwmb_meta( 'services_pilars_text_2' ); ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </article>
+
+            <?php endwhile; ?>
 
         </main>
     </div>
--- template-parts/tourism.php
+++ template-parts/tourism.php
@@ -7,12 +7,18 @@ get_header(); ?>
     <div id="primary" class="content-area">
         <main id="main" class="site-main" role="main">
 
-            <?php
-            while ( have_posts() ) : the_post();
+            <?php while ( have_posts() ) : the_post(); ?>
 
-                get_template_part( 'template-parts/content', 'page-tourism' );
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <header class="entry-header">
+                    <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+                </header>
+                <div class="entry-content">
+                    <?php the_content(); ?>
+                </div>
+            </article>
 
-            endwhile; ?>
+            <?php endwhile; ?>
 
         </main>
     </div>
