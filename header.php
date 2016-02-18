ff1506258083f77cb9c9969f03dd387105729923
Martin Luna <martinluna@uxmoon.com>
2016-02-18 11:27:51 -0300

    updated header markup

--- header.php
+++ header.php
@@ -16,27 +16,13 @@
 <div id="page" class="site">
     <header id="masthead" class="site-header" role="banner">
         <div class="site-branding">
-
-            <?php
-            if ( is_front_page() && is_home() ) : ?>
-                <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-            <?php else : ?>
-                <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-            <?php
-            endif;
-
-            $description = get_bloginfo( 'description', 'display' );
-            if ( $description || is_customize_preview() ) : ?>
-                <p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-            <?php
-            endif; ?>
+            <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?>. <?php bloginfo( 'description' ); ?>.</a></h1>
         </div>
 
         <nav id="site-navigation" class="main-navigation" role="navigation">
             <?php wp_nav_menu(
                 array(
                     'theme_location' => 'primary',
-                    'menu_id'        => 'primary-menu',
                     'container'      => '',
                     'menu_class'     => 'site-menu'
                     )
