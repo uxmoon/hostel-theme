f3f79ec895548e74f68623140280e78c7794094a
Martin Luna <martinluna@uxmoon.com>
2016-02-18 12:22:46 -0300

    added util template page for ui components and layout

---
+++ template-parts/util.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Template Name: Util
+*/
+get_header(); ?>
+
+<div class="row">
+    <h3>.sidebar+.column</h3>
+    <div class="sidebar">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae a repellendus unde in magni porro. Nobis quibusdam possimus cumque blanditiis, molestias praesentium eos architecto dicta esse quo placeat, culpa repellat?</div>
+    <div class="column">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos voluptate, eius recusandae laborum corrupti, illum doloremque? Inventore doloribus magni blanditiis maiores eius officiis ullam ratione porro, perspiciatis dolores maxime soluta?</div>
+</div>
+
+<div class="row">
+    <h3>.half</h3>
+    <div class="half">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae a repellendus unde in magni porro. Nobis quibusdam possimus cumque blanditiis, molestias praesentium eos architecto dicta esse quo placeat, culpa repellat?</div>
+    <div class="half">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos voluptate, eius recusandae laborum corrupti, illum doloremque? Inventore doloribus magni blanditiis maiores eius officiis ullam ratione porro, perspiciatis dolores maxime soluta?</div>
+</div>
+<div class="row">
+    <h3>.row</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla possimus porro non sint aspernatur magni. Id, vitae iusto earum ea perferendis quod laborum optio repellendus error quo molestias similique. Ex.</p>
+</div>
+
+<?php get_footer(); ?>
