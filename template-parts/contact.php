92a4581bfdf3d848ade547feaeeb55080b4c98d4
Martin Luna <martinluna@uxmoon.com>
2016-02-26 20:46:23 -0300

    updated contact form

--- template-parts/contact.php
+++ template-parts/contact.php
@@ -22,25 +22,25 @@ get_header(); ?>
                 <header class="entry-header">
                     <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
                 </header>
-                <div class="entry-content">
 
+                <div class="entry-content">
                 <?php
                 $field_phone = 'general_phone';
                 if ( isset( $settings[$field_phone] ) )
                 {
-                    echo '<p>Puede contactarnos a los siguientes teléfonos:' . $settings[$field_phone];
+                    echo '<p>Puede contactarnos a los siguientes teléfonos: <strong class="text-phone">Fijo: ' . $settings[$field_phone];
                 }
 
                 $field_cellphone = 'general_cellphone';
                 if ( isset( $settings[$field_cellphone] ) )
                 {
-                    echo $settings[$field_cellphone] . '</p>';
+                    echo ' Celular: ' . $settings[$field_cellphone] . '</strong></p>';
                 }
 
 
                 $field_email = 'general_email';
                 if ( isset( $settings[$field_email] ) ) {
-                    echo '<p>O enviarnos un mail a: ' . $settings[$field_email] . '</p>';
+                    echo '<p>O enviarnos un mail a: <strong class="text-email">' . $settings[$field_email] . '</strong></p>';
                 }
                 ?>
 
