d419d64d9a3fe72fc2463a856b7f9196fd367a51
Martin Luna <martinluna@uxmoon.com>
2016-02-27 21:18:39 -0300

    updated rates page

--- template-parts/rates.php
+++ template-parts/rates.php
@@ -14,22 +14,40 @@ get_header(); ?>
                     <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
                 </header>
                 <div class="entry-content">
-                    <h2>Precios por día</h2>
-                    <p>Habitación Doble <?php echo rwmb_meta( 'rates_double_room_rate' ); ?></p>
-                    <p>Habitación Triple <?php echo rwmb_meta( 'rates_triple_room_rate' ); ?></p>
-                    <p>Habitación Cuádruple <?php echo rwmb_meta( 'rates_quadruple_room_rate' ); ?></p>
-                    <p>Check In: <?php echo rwmb_meta( 'rates_checkin_hour' ); ?></p>
-                    <p>Check Out: <?php echo rwmb_meta( 'rates_checkout_hour' ); ?></p>
-                    <p>Late Check Out: <?php echo rwmb_meta( 'rates_late_checkout' ); ?></p>
-                    <p><?php echo rwmb_meta( 'rates_restrictions' ); ?></p>
+                    <div class="hosteria-rates">
+                        <h2>Precios por día</h2>
+                        <div class="row">
+                            <div class="one-third">
+                                <p>Habitación Doble <strong><?php echo rwmb_meta( 'rates_double_room_rate' ); ?></strong></p>
+                            </div>
+                            <div class="one-third">
+                                <p>Habitación Triple <strong><?php echo rwmb_meta( 'rates_triple_room_rate' ); ?></strong></p>
+                            </div>
+                            <div class="one-third">
+                                <p>Habitación Cuádruple <strong><?php echo rwmb_meta( 'rates_quadruple_room_rate' ); ?></strong></p>
+                            </div>
+                        </div>
+                    </div>
 
-                    <p>Si quiere realizar una reserva o hacernos alguna consulta puede <a href="<?php echo esc_url( home_url( '/' ) ); ?>/contacto">contactarnos</a></p>
+                    <div class="row">
+                        <div class="hosteria-hours half">
+                            <p>Check In: <?php echo rwmb_meta( 'rates_checkin_hour' ); ?> - Check Out: <?php echo rwmb_meta( 'rates_checkout_hour' ); ?></p>
+                            <p><strong>Late Check Out:</strong> <?php echo rwmb_meta( 'rates_late_checkout' ); ?></p>
+                        </div>
+                        <div class="hosteria-restrictions half">
+                            <p><?php echo rwmb_meta( 'rates_restrictions' ); ?></p>
+                        </div>
+                    </div>
+
                 </div>
             </article>
 
             <?php endwhile; ?>
 
         </main>
+        <div class="hosteria-contact-us">
+            <p>Si quiere realizar una reserva o hacernos alguna consulta puede <a href="<?php echo esc_url( home_url( '/' ) ); ?>/contacto" class="button">contactarnos</a></p>
+        </div>
     </div>
 
 <?php get_footer(); ?>
