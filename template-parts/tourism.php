cd6fd0d7a9be59e6abe61ae92573979963bbded3
Martin Luna <martinluna@uxmoon.com>
2016-03-19 20:52:01 -0300

    updated tourism layout

--- template-parts/tourism.php
+++ template-parts/tourism.php
@@ -4,7 +4,7 @@ Template Name: Tourism
 */
 get_header(); ?>
 
-<div id="primary" class="content-area">
+<div id="primary" class="content-area page-tourism">
   <main id="main" class="site-main row" role="main">
 
     <?php while ( have_posts() ) : the_post(); ?>
